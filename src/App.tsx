2c85d7ff543ebde4cdb495a9f696c16504a60945
Jean Santos <jeansantos.sw@gmail.com>
2025-08-05 11:17:23 +0100

    Feature: Config AXIOS e REACT-QUERY

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import '@/styles/global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './http/services/react-query/react-query'
 import { routes } from './router/routes'
 
 export function App() {
   return (
     <ThemeProvider storageKey="fooddelivery-theme" defaultTheme="system">
       <Toaster richColors />
-      <RouterProvider router={routes} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={routes} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
