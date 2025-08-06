e68beefe08b602d6e2805e79ffd7b23eeb0a58f6
Jean Santos <jeansantos.sw@gmail.com>
2025-08-06 20:31:04 +0100

    Feature: Card total de pedidos no dia

--- src/page/app/dashboard/components/cards/day-orders-amount-card.tsx
+++ src/page/app/dashboard/components/cards/day-orders-amount-card.tsx
@@ -1,8 +1,45 @@
 import { Utensils } from 'lucide-react'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { useQuery } from '@tanstack/react-query'
+import { getDayOrdersAmount } from '@/api/http/services/order-metrics-dashboard/get-day-orders-amount'
 
 export function DayOrdersAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryKey: ['metrics', 'day-orders-amount'],
+    queryFn: getDayOrdersAmount
+  })
+
+  if (!dayOrdersAmount) {
+    return
+  }
+
+  let dailyPercentageOfTotalOrders = null
+
+  if (dayOrdersAmount?.diffFromYesterday > 0) {
+    dailyPercentageOfTotalOrders = (
+      <p className="to-muted-foreground text-xs">
+        <span className="text-shadow-emerald-700 dark:text-emerald-600">+{dayOrdersAmount.diffFromYesterday}% </span>
+        em relação ao dia anterior
+      </p>)
+  }
+
+  if (dayOrdersAmount?.diffFromYesterday === 0) {
+    dailyPercentageOfTotalOrders = (
+      <p className="to-muted-foreground text-xs">
+        <span className="text-yellow-500 dark:text-yellow-400">{dayOrdersAmount.diffFromYesterday}% </span>
+        em relação ao dia anterior
+      </p>)
+  }
+
+  if (dayOrdersAmount?.diffFromYesterday < 0) {
+    dailyPercentageOfTotalOrders = (
+      <p className="to-muted-foreground text-xs">
+        <span className="text-red-500 dark:text-red-400">-{dayOrdersAmount.diffFromYesterday}% </span>
+        em relação ao dia anterior
+      </p>)
+  }
+
   return (
     <Card>
       <CardHeader className="flex items-center justify-between pb-2">
@@ -12,11 +49,12 @@ export function DayOrdersAmountCard() {
         <Utensils className="text-muted-foreground h-4 w-4" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">30</span>
-        <p className="to-muted-foreground text-xs">
-          <span className="text-yellow-500 dark:text-yellow-400">-1% </span>
-          em relação ao dia anterior
-        </p>
+        {dayOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">{dayOrdersAmount.amount.toLocaleString('pt-PT')}</span>
+            {dailyPercentageOfTotalOrders}
+          </>
+        )}
       </CardContent>
     </Card>
   )
--- src/page/app/dashboard/dashboard.tsx
+++ src/page/app/dashboard/dashboard.tsx
@@ -10,10 +10,10 @@ export function Dasboard() {
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
       <div className="grid grid-cols-4 gap-4">
-        <MonthlyPrescriptionCard />
-        <MonthlyOrdersAmountCard />
-        <DayOrdersAmountCard />
         <MonthlyCanceledOrdersAmountCard />
+        <DayOrdersAmountCard />
+        <MonthlyOrdersAmountCard />
+        <MonthlyPrescriptionCard />
       </div>
       <div className="grid grid-cols-9 gap-4">
         <RevenueChat />
