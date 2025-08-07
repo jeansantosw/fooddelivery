360dfe566446bd3c50175812456342ad2579922f
Jean Santos <jeansantos.sw@gmail.com>
2025-08-07 14:06:50 +0100

    Feature: Ajuste test componente (order-status)

--- src/page/app/orders/components/order-status.spec.tsx
+++ src/page/app/orders/components/order-status.spec.tsx
@@ -4,56 +4,46 @@ import { OrderStatus } from './order-status'
 describe('Order status', () => {
   it('should show exact text in (pending) order status', () => {
 
-    const wrapper = render(<OrderStatus status='pending' />)
-    const statusTesxt = wrapper.getByText('Pendente')
-    const statusBadElement = wrapper.getByTestId('badge')
+    const { getByText, getByTestId } = render(<OrderStatus status='pending' />)
 
-    expect(statusTesxt).toBeInTheDocument()
-    expect(statusBadElement).toHaveClass('bg-zinc-500')
+    expect(getByText('Pendente')).toBeInTheDocument()
+    expect(getByTestId('badge')).toHaveClass('bg-zinc-500')
 
   })
 
   it('should show exact text in (processing) order status', () => {
 
-    const wrapper = render(<OrderStatus status='processing' />)
-    const statusTesxt = wrapper.getByText('Preparando pedido')
-    const statusBadElement = wrapper.getByTestId('badge')
+    const { getByText, getByTestId } = render(<OrderStatus status='processing' />)
 
-    expect(statusTesxt).toBeInTheDocument()
-    expect(statusBadElement).toHaveClass('bg-amber-400')
+    expect(getByText('Preparando pedido')).toBeInTheDocument()
+    expect(getByTestId('badge')).toHaveClass('bg-amber-400')
 
   })
 
   it('should show exact text in (delivering) ordeer status', () => {
 
-    const wrapper = render(<OrderStatus status='delivering' />)
-    const statusTesxt = wrapper.getByText('Em entrega')
-    const statusBadElement = wrapper.getByTestId('badge')
+    const { getByText, getByTestId } = render(<OrderStatus status='delivering' />)
 
-    expect(statusTesxt).toBeInTheDocument()
-    expect(statusBadElement).toHaveClass('bg-blue-500')
+    expect(getByText('Em entrega')).toBeInTheDocument()
+    expect(getByTestId('badge')).toHaveClass('bg-blue-500')
 
   })
 
   it('should show exact text in (delivered) order status', () => {
 
-    const wrapper = render(<OrderStatus status='delivered' />)
-    const statusTesxt = wrapper.getByText('Entregue')
-    const statusBadElement = wrapper.getByTestId('badge')
+    const { getByText, getByTestId } = render(<OrderStatus status='delivered' />)
 
-    expect(statusTesxt).toBeInTheDocument()
-    expect(statusBadElement).toHaveClass('bg-green-800')
+    expect(getByText('Entregue')).toBeInTheDocument()
+    expect(getByTestId('badge')).toHaveClass('bg-green-800')
 
   })
 
   it('should show exact text in (canceled) order status', () => {
 
-    const wrapper = render(<OrderStatus status='canceled' />)
-    const statusTesxt = wrapper.getByText('Cancelado')
-    const statusBadElement = wrapper.getByTestId('badge')
+    const { getByText, getByTestId } = render(<OrderStatus status='canceled' />)
 
-    expect(statusTesxt).toBeInTheDocument()
-    expect(statusBadElement).toHaveClass('bg-red-800')
+    expect(getByText('Cancelado')).toBeInTheDocument()
+    expect(getByTestId('badge')).toHaveClass('bg-red-800')
 
   })
 })
